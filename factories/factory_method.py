ad5e864917c60164a8176a6c584ae85d022f2123
maheshvakkund <maheshvakkund3107@outlook.com>
2021-12-20 20:12:20 +0530

    -Factory Method Design Pattern.

--- factories/factory_method.py
+++ factories/factory_method.py
@@ -3,25 +3,30 @@ from cmath import cos, sin
 
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
-    @staticmethod
-    def new_cartesian_point(x, y):
-        return Point(x, y)
+    # PointFactory is a inner class here
+    class PointFactory:
+        @staticmethod
+        def new_cartesian_point(x, y):
+            p = Point()
+            p.x = x
+            p.y = y
+            return p
 
-    @staticmethod
-    def new_polar_point(rho, theta):
-        return Point(rho * cos(theta), rho * sin(theta))
+        @staticmethod
+        def new_polar_point(rho, theta):
+            return Point(rho * cos(theta), rho * sin(theta))
 
-    def __str__(self):
-        return f"x:{self.x}, y:{self.y}"
+        def __str__(self):
+            return f"x:{self.x}, y:{self.y}"
 
 
 if __name__ == '__main__':
     p = Point(2, 3)
-    pe = Point.new_polar_point(5, 7)
-    ab = Point.new_cartesian_point(4, 6)
+    pe = Point.PointFactory.new_polar_point(5, 7)
+    ab = Point.PointFactory.new_cartesian_point(4, 6)
     print(pe)
     print(ab)
